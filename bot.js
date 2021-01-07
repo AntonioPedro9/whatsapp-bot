e29ea56a2eedf6deaac4b47127e9ed5096cbdeec
Antônio Pedro <antoniopedro.rs9@gmail.com>
2021-01-07 18:51:06 -0300

    Code structure modification

--- bot.js
+++ bot.js
@@ -1,46 +1,44 @@
 const puppeteer = require("puppeteer");
 const prompt = require("prompt-sync")();
 
-let browser, page, chatName;
-
 (async function main() {
 
-	// Get the whatsapp chat name:
+	// Get whatsapp chat name:
 	do {
-		chatName = prompt("Write exactly the name of the chat you want to spam: ");
+		var chatName = prompt("Write exactly the name of the chat you want to spam: ");
 	} while (!chatName);
 
 	// Launch browser:
-	browser = await puppeteer.launch({ headless: false });
-	page = await browser.newPage();
+	const browser = await puppeteer.launch({ headless: false });
+	const page = await browser.newPage();
 
 	// Navigate to whatsapp:
 	await page.goto("https://web.whatsapp.com/", { waitUntil: "domcontentloaded" });
 
-	selectChat(chatName);
-	sendMessage(message);
-})();
-
-async function selectChat(chatName) {
-	await page.waitForSelector("div[data-tab='3']");                 // wait search input load
-	await page.type("div[data-tab='3']", chatName);                  // type chat name at input
-	await page.waitForSelector("span[class='matched-text _1VzZY']"); // wait chat message load
-	await page.click("span[class='matched-text _1VzZY']");           // click at chat message
-}
-
-async function sendMessage(message) {
-	await page.waitForSelector("div[data-tab='6']"); // wait message input load
-
-	let lines = message.split("\n");
-	let i = 0;
-
-	while (i < lines.length && lines[i].trim() != "") {
-		await page.type("div[data-tab='6']", message); // type the message at the input
-		await page.click("span[data-testid='send']");  // click at send button
-
-		i++
+	async function selectChat(chatName) {
+		await page.waitForSelector("div[data-tab='3']");                 // wait search input load
+		await page.type("div[data-tab='3']", chatName);                  // type chat name at input
+		await page.waitForSelector("span[class='matched-text _1VzZY']"); // wait chat message load
+		await page.click("span[class='matched-text _1VzZY']");           // click at chat message
 	}
-}
+	selectChat(chatName);
+
+	async function sendMessage(message) {
+		await page.waitForSelector("div[data-tab='6']"); // wait message input load
+
+		let lines = message.split("\n");
+		let i = 0;
+
+		while (i < lines.length && lines[i].trim() != "") {
+			await page.type("div[data-tab='6']", message); // type the message at the input
+			await page.click("span[data-testid='send']");  // click at send button
+
+			i++
+		}
+	}
+	sendMessage(message);
+	
+})();
 
 const message = `No princípio, Deus criou os céus e a terra.
 A terra estava informe e vazia; as trevas cobriam o abismo e o Espírito de Deus pairava sobre as águas.
