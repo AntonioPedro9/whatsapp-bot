f155a3678effa767894de22b2dd3e2e39ad5f0e6
Antônio Pedro <antoniopedro.rs9@gmail.com>
2021-01-13 19:25:23 -0300

    Code readability improvements

--- src/bot.js
+++ src/bot.js
@@ -2,41 +2,52 @@ const puppeteer = require("puppeteer");
 const prompt = require("prompt-sync")();
 const message = require("./message");
 
+let browser, page;
+
 (async function main() {
 
 	// Get whatsapp chat name:
-	do {
-		var chatName = prompt("Write exactly the name of the chat you want to spam: ");
-	} while (!chatName);
+	do var chatName = prompt("Write exactly the name of the chat you want to spam: ");
+	while (!chatName);
 
 	// Launch browser:
-	const browser = await puppeteer.launch({ headless: false });
-	const page = await browser.newPage();
+	browser = await puppeteer.launch({ headless: false, args: ["--start-maximized"] });
+	page = await browser.newPage();
+	page.setViewport({ width: 1920, height: 1080 });
 
 	// Navigate to whatsapp:
 	await page.goto("https://web.whatsapp.com/", { waitUntil: "domcontentloaded" });
 
-	async function selectChat(chatName) {
-		await page.waitForSelector("div[data-tab='3']");                 // wait search input load
-		await page.type("div[data-tab='3']", chatName);                  // type chat name at input
-		await page.waitForSelector("span[class='matched-text _1VzZY']"); // wait chat message load
-		await page.click("span[class='matched-text _1VzZY']");           // click at chat message
-	}
-	selectChat(chatName);
+	await typeIn("div[data-tab='3']", chatName);        // type chat name in the input
+	await clickAt("span[class='matched-text _1VzZY']"); // click at chat message
 
-	async function sendMessage(message) {
-		await page.waitForSelector("div[data-tab='6']"); // wait message input load
-
-		let lines = message.split("\n");
-		let i = 0;
-
-		while (i < lines.length && lines[i].trim() != "") {
-			await page.type("div[data-tab='6']", message); // type the message at the input
-			await page.click("span[data-testid='send']");  // click at send button
-
-			i++
-		}
-	}
 	sendMessage(message);
-	
 })();
+
+
+
+async function sendMessage(message) {
+	let lines = message.split("\n");
+	let i = 0;
+
+	while (i < lines.length && lines[i].trim() != "") {
+		await typeIn("div[data-tab='6']", message); // type message in the input
+		await clickAt("span[data-testid='send']");  // click at send button
+
+		i++
+	}
+}
+
+
+
+async function clickAt(element) {
+	await page.waitForSelector(`${element}`);
+	await page.click(`${element}`);
+}
+
+
+
+async function typeIn(element, text) {
+	await page.waitForSelector(`${element}`);
+	await page.type(`${element}`, text);
+}
